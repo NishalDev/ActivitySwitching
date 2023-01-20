98e746d27cd4ac8e39ef7805a8a15c83e4bfdd16
Nishal <nishaldevadiga2003@gmail.com>
2023-01-20 18:39:55 +0530

    Seventh Commit

--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -21,24 +21,36 @@ object DataService {
     )
 
     val hats= listOf(
-        Product("Infi Cap","4$","hat01"),
-        Product("Infi Cap1","8$","hat02"),
-        Product("Infi Cap2","10$","hat03"),
-        Product("Infi Cap3","12$","hat04")
+        Product("Infi Cap","4$","hat1"),
+        Product("Infi Cap1","8$","hat2"),
+        Product("Infi Cap2","10$","hat3"),
+        Product("Infi Cap3","12$","hat4")
     )
 
     val hoodies = listOf(
-        Product("Infi Hoodie1","$20","hoodie01"),
-        Product("Infi Hoodie2","$24","hoodie02"),
-        Product("Infi Hoodie3","$28","hoodie03"),
-        Product("Infi Hoodie4","$30","hoodie04")
+        Product("Infi Hoodie1","$20","hoodie1"),
+        Product("Infi Hoodie2","$24","hoodie2"),
+        Product("Infi Hoodie3","$28","hoodie3"),
+        Product("Infi Hoodie4","$30","hoodie4")
     )
 
     val shirts = listOf(
-        Product("Infi Shirt1","$40","shirt01"),
-        Product("Infi Shirt2","$42","shirt02"),
-        Product("Infi Shirt3","$44","shirt03"),
-        Product("Infi Shirt4","$46","shirt04")
+        Product("Infi Shirt1","$40","shirt1"),
+        Product("Infi Shirt2","$42","shirt2"),
+        Product("Infi Shirt3","$44","shirt3"),
+        Product("Infi Shirt4","$46","shirt4")
     )
+
+    val digitalGood = listOf<Product>()
+
+    fun getProducts(category: String) : List<Product> {
+
+        return when(category){
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
+        }
+    }
 
 }
