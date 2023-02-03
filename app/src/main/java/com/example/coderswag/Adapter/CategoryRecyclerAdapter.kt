8c2697d5cee9cac0dad0c02567c50a1929b5dd1e
Nishal <nishaldevadiga2003@gmail.com>
2023-02-03 23:46:35 +0530

    reformat code

--- app/src/main/java/com/example/coderswag/Adapter/CategoryRecyclerAdapter.kt
+++ app/src/main/java/com/example/coderswag/Adapter/CategoryRecyclerAdapter.kt
@@ -11,16 +11,22 @@ import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import com.google.android.material.transition.Hold
 
-class CategoryRecyclerAdapter(val context : Context, val categories : List<Category>, val itemClick: (Category) -> Unit) : RecyclerView.Adapter<CategoryRecyclerAdapter.Holder>() {
+class CategoryRecyclerAdapter(
+    val context: Context,
+    val categories: List<Category>,
+    val itemClick: (Category) -> Unit
+) : RecyclerView.Adapter<CategoryRecyclerAdapter.Holder>() {
 
 
-    inner class Holder(itemView: View, val itemClick: (Category) -> Unit) : RecyclerView.ViewHolder(itemView) {
+    inner class Holder(itemView: View, val itemClick: (Category) -> Unit) :
+        RecyclerView.ViewHolder(itemView) {
 
         val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
         val categoryName = itemView?.findViewById<TextView>(R.id.categoryname)
 
-        fun bindCategory(category : Category, context: Context){
-            val resourceId = context.resources.getIdentifier(category.image, "drawable",context.packageName)
+        fun bindCategory(category: Category, context: Context) {
+            val resourceId =
+                context.resources.getIdentifier(category.image, "drawable", context.packageName)
             categoryImage?.setImageResource(resourceId)
             categoryName?.text = category.title
             itemView.setOnClickListener { itemClick(category) }
@@ -29,7 +35,8 @@ class CategoryRecyclerAdapter(val context : Context, val categories : List<Categ
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.catergory_list_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.catergory_list_item, parent, false)
         return Holder(view, itemClick)
     }
 
